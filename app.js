e9e80f78ac9c02906ec7341b0cf1df6500f2df4c
Ubuntu <ubuntu@ip-172-31-80-210.ec2.internal>
2022-02-04 10:14:13 +0000

    changes made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey, i have amde the changes to this file'));
+app.get('/', (req, res) => res.send('Hey, new changes have made successfully'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
